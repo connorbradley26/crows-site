51bf74a2d45a5089f583c6c41d653e77d9489797
connor <c.bradley.dev@gmail.com>
2025-11-24 21:13:06 +0000

    tidied up

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'jersey-m54': ['Jersey M54', 'sans-serif'],
+				'geist': ['Geist', 'sans-serif'],
+			},
+			colors: {
+				'crows-red': '#52102d',
+				'crows-red-hover': '#7a1a48',
+			},
+		},
 	},
 	plugins: [],
 }
