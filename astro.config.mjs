cd4781c266603fdb8bceadba0507cd8ca6ed5580
Connor Bradley <c.bradley.dev@gmail.com>
2025-11-24 20:09:15 +0000

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [tailwind(), sitemap(), robotsTxt()],
+    output: "server",
+    adapter: vercel({
+        speedInsights: {
+            enabled: true,
+        },
+    }),
+});
