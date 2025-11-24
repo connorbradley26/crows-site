f1225d1e1a2ef9182529d93d3df6e36d877ec19b
connor <c.bradley.dev@gmail.com>
2025-11-24 21:24:50 +0000

    fixed build isues

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
-
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), sitemap(), robotsTxt()],
-    output: "server",
-    adapter: vercel({
-        speedInsights: {
-            enabled: true,
-        },
-    }),
 });
